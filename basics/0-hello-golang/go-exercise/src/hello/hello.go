33a4feef66da5507bb24b95fd336869f7cc6ac87
Yi-Ting Li <liyitingtom@gmail.com>
2022-05-27 23:10:07 +0800

    add: 0-hello-golang
    The dir contains 1 dir and 1 markdown file,
    the `go-exercise` dir is a illustration for `Package and $GOPATH`
    subject in `hello_go.md` file.

---
+++ basics/0-hello-golang/go-exercise/src/hello/hello.go
@@ -0,0 +1,8 @@
+// The hello.go script for illustrating for package_management.md
+package hello
+
+import "fmt"
+
+func HelloGo() { // The function name must start with an uppercase letter.
+	fmt.Println("Hello Golang!")
+}
---
+++ basics/0-hello-golang/go-exercise/src/main/main.go
@@ -0,0 +1,8 @@
+// The main.go script for illustrating for package_management.md
+package main
+
+import "hello" // Import the `hello` package.
+
+func main() {
+	hello.HelloGo() // Using the HelloGo() function in hello package.
+}
