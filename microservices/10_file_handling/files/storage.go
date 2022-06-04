fc59464de19eb1e69497fbd2aa1a77ddf9ce2e5a
Yi-Ting Li <liyitingtom@gmail.com>
2022-06-04 14:20:55 +0800

    feat: add Get function

--- microservices/10_file_handling/files/storage.go
+++ microservices/10_file_handling/files/storage.go
@@ -1,8 +1,12 @@
 package files
 
-import "io"
+import (
+	"io"
+	"os"
+)
 
 // Storage defines the behavior for file operations.
 type Storage interface {
 	Save(path string, contents io.Reader) error
+	Get(path string) (*os.File, error)
 }
