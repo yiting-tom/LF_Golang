4d98d7b770b62644d8aad8c3a480a837496e5d6c
Yi-Ting Li <liyitingtom@gmail.com>
2022-06-04 14:25:29 +0800

    refactor: depart the files.go into methods files
    depart the files.go file into get.go and post.go

--- microservices/10_file_handling/handlers/files.go
+++ microservices/10_file_handling/handlers/files.go
@@ -2,10 +2,7 @@ package handlers
 
 import (
 	"10_file_handling/files"
-	"net/http"
-	"path/filepath"
 
-	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -18,26 +15,4 @@ type Files struct {
 // NewFiles creates a new Files handler.
 func NewFiles(l *log.Logger, s files.Storage) *Files {
 	return &Files{l, s}
-}
-
-// ServeHTTP implements the http.Handler interface.
-func (f *Files) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	fn := vars["filename"]
-
-	f.l.Info("<POST> received request for file: ", id, fn)
-
-	f.saveFile(id, fn, w, r)
-}
-
-// saveFile saves the contents of the request body to the file.
-func (f *Files) saveFile(id, path string, w http.ResponseWriter, r *http.Request) {
-	f.l.Info("Save file: ", id, path)
-
-	fp := filepath.Join(id, path)
-	if err := f.store.Save(fp, r.Body); err != nil {
-		f.l.Error("Failed to save file: ", err)
-		http.Error(w, "Failed to save file", http.StatusInternalServerError)
-	}
 }
