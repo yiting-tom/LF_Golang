af526783a80291463741d361aa00b5e28f8a6781
Yi-Ting Li <liyitingtom@gmail.com>
2022-06-21 00:21:58 +0800

    add: data/products.go for product API

---
+++ microservices/project/product-api/data/products.go
@@ -0,0 +1,45 @@
+package data
+
+import (
+	"fmt"
+)
+
+// ErrProductNotFound is used when a product is not found in db
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+// Product defines the structure for an API product
+// swagger:model
+type Product struct {
+	// the id for the product
+	//
+	// required: false
+	// min: 1
+	ID int `json:"id"` // Unique identifier for the product
+
+	// the name for this product
+	//
+	// required: true
+	// max length: 255
+	Name string `json:"name" validate:"required"`
+
+	// the description for this product
+	//
+	// required: false
+	// max length: 10000
+	Description string `json:"description"`
+
+	// the price for the product
+	//
+	// required: true
+	// min: 0.01
+	Price float32 `json:"price" validate:"required,gt=0"`
+
+	// the SKU for the product
+	//
+	// required: true
+	// pattern: [a-z]+-[a-z]+-[a-z]+
+	SKU string `json:"sku" validate:"sku"`
+}
+
+// Products defines a slice of Product
+type Products []*Product
