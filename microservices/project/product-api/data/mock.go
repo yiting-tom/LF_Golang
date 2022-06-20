5e37a468ff226224efb3ff927a77f76c8db70ad7
Yi-Ting Li <liyitingtom@gmail.com>
2022-06-21 00:22:17 +0800

    add: data/mock.go for mocking the products in database

---
+++ microservices/project/product-api/data/mock.go
@@ -0,0 +1,81 @@
+package data
+
+var productList = Products{
+	{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc-123-abc",
+	},
+	{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "def-456-def",
+	},
+}
+
+// GetProducts returns a list of all products in the database
+func GetProducts() Products {
+	return productList
+}
+
+// GetProductByID returns a product from the database by id
+// Returns ErrProductNotFound when no product can be found
+func GetProductByID(id int) (*Product, error) {
+	i := findIndexByProductID(id)
+	if id == -1 {
+		return nil, ErrProductNotFound
+	}
+
+	return productList[i], nil
+}
+
+// UpdateProduct updates a product in the database
+// Returns ErrProductNotFound when no product can be found
+func UpdateProduct(p Product) error {
+	i := findIndexByProductID(p.ID)
+	if i == -1 {
+		return ErrProductNotFound
+	}
+
+	// update the product in the DB
+	productList[i] = &p
+
+	return nil
+}
+
+// AddProduct adds a product to the database
+func AddProduct(p Product) {
+	// get the next id in sequence
+	maxID := productList[len(productList)-1].ID
+	p.ID = maxID + 1
+	productList = append(productList, &p)
+}
+
+// DeleteProduct deletes a product from the database
+// Returns ErrProductNotFound when no product can be found
+func DeleteProduct(id int) error {
+	i := findIndexByProductID(id)
+	if i == -1 {
+		return ErrProductNotFound
+	}
+
+	productList = append(productList[:i], productList[i+1])
+
+	return nil
+}
+
+// findIndexByProductID finds the index of a product by id
+// Returns -1 when no product can be found
+func findIndexByProductID(id int) int {
+	for i, p := range productList {
+		if p.ID == id {
+			return i
+		}
+	}
+
+	return -1
+}
