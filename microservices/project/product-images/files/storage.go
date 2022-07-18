ca38b8669c2aa0bd60097bd9aae02c72cff7eb79
Yi-Ting Li <liyitingtom@gmail.com>
2022-07-18 11:26:09 +0800

    feat: add Get interface

--- microservices/project/product-images/files/storage.go
+++ microservices/project/product-images/files/storage.go
@@ -1,8 +1,12 @@
 package files
 
-import "io"
+import (
+	"io"
+	"os"
+)
 
 // Storage defines the interface for file operations.
 type Storage interface {
 	Save(path string, contents io.Reader) error
+	Get(path string) (*os.File, error)
 }
