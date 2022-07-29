2b9d5bf5c06506caff415de776942465c8b1e7ae
Yi-Ting Li <liyitingtom@gmail.com>
2022-07-30 00:07:56 +0800

    add: the client main.go

---
+++ gRPC/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	pb "github.com/yiting-tom/LF_Golang/grpc/route"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// Dial is a convenience function for dialing
+	// a client connection to a server.
+	// the first argument is the target server address,
+	// and others are dial options.
+	conn, err := grpc.Dial(
+		"localhost:5000",
+		grpc.WithInsecure(), // Since we don't have server yet
+		grpc.WithBlock(),    // Block until the connection is established
+	)
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewRouteGuideClient(conn)
+	feature, err := client.GetFeature(
+		context.Background(),
+		&pb.Point{
+			Latitude:  37427034,
+			Longitude: 12212124,
+		},
+	)
+	if err != nil {
+		log.Fatalf("failed to get feature: %v", err)
+	}
+
+	fmt.Println(feature)
+}
